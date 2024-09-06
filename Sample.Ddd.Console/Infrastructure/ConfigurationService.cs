6df6d1d882b127f9d5ad942ccf28c2267519f1ac
Filipe <filipe.ssilva97@gmail.com>
2024-09-06 12:28:46 -0300

    Program + ConfigService

---
+++ Sample.Ddd.Console/Infrastructure/ConfigurationService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sample.Ddd.Data.Repositories;
+using Sample.Ddd.Domain.Repositories;
+using Sample.Ddd.Service.Services;
+using Sample.Ddd.Service.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Ddd.Console.Infrastructure
+{
+    internal class ConfigurationService
+    {
+        public static void ConfigureServices(IServiceCollection services) { //1
+
+            //service
+            services.AddTransient<ISampleService, SampleService>();//2
+
+            //repository/data
+            services.AddTransient<ISampleRepository, SampleRepository>();//3
+        }
+    }
+}
--- Sample.Ddd.Console/Program.cs
+++ Sample.Ddd.Console/Program.cs
@@ -1,9 +1,21 @@
-﻿using Sample_Ddd_Aplication;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sample.Ddd.Service.Services.Interfaces;
+using Sample_Ddd_Aplication;
 
 namespace Sample.Ddd.Console
 {
     public class Program
     {
-        
+        public string Main()
+        {
+            ServiceCollection services = new ServiceCollection();
+            var inject = services.BuildServiceProvider();
+            BusinessIntegration integration = new BusinessIntegration(inject.GetService<ISampleService>());
+            var result = integration.BusinessIntegrationTeste();
+            System.Console.WriteLine(result);
+            return result;
+        }
+
+
     }
 }
